f13c7a0a460b715694584df09b4d6b133f338ff0
Alonzo Jackson <jaxonetic@gmail.com>
2021-05-02 17:08:42 -0700

    adding travis.xml

--- __tests__/dailyMantrasSpec.js
+++ __tests__/dailyMantrasSpec.js
@@ -6,7 +6,7 @@ import 'react-native';
 import React from 'react';
 import DailyMantras from '../src/dailyMantras/dailyMantras.js';
 import { render } from '@testing-library/react-native';
-import {TXT_DAY_OF_WEEK} from '../src/constants.js';
+import {DAILY_MANTRA_DATA} from '../src/constants.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -18,12 +18,12 @@ it('Displays days and mantras correctly', () => {
   //Ensure Title Displays
   const beejs = queryAllByText('Beej');
   console.log(beejs.length);
-console.log(TXT_DAY_OF_WEEK);
+console.log(DAILY_MANTRA_DATA);
 //console.log(beejs[0].type);
   const titlt = queryAllByText('Daily');
 
   //Ensure Days of the week print, Sun to Sat
-  getByText(TXT_DAY_OF_WEEK[0]);getByText(TXT_DAY_OF_WEEK[1]);getByText(TXT_DAY_OF_WEEK[2]);getByText(TXT_DAY_OF_WEEK[3]);getByText(TXT_DAY_OF_WEEK[4]);getByText(TXT_DAY_OF_WEEK[5]);getByText(TXT_DAY_OF_WEEK[6]);
+  getByText(DAILY_MANTRA_DATA[0].day);getByText(DAILY_MANTRA_DATA[1].day);getByText(DAILY_MANTRA_DATA[2]);getByText(DAILY_MANTRA_DATA[3]);getByText(DAILY_MANTRA_DATA[4]);getByText(DAILY_MANTRA_DATA[5]);getByText(DAILY_MANTRA_DATA[6]);
   
   //console.log(element);
  // expect(element).toBe('Sunday');
--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,22 @@ export const TXT_MANTRAS_HOWTOUSE = 'The can be used to focus the mind and how w
 */
 //Array of Days, 8 Based
 export const TXT_DAY_OF_WEEK = ['Sunday','Monday','Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday'];
+export const TITLE = 'Daily Mantras';
+export const DAILY_MANTRA_DATA = [
+{id:0, day:'Sunday', graha:'Sun / Surya', mantras:{beej:'\t\tOm Kraam Kreem Kraum Saha,\n\t\t\t\tSuryaya Namaha', alt:'Om Suryaya Namaha', translation:'', historic:{mantra:'agnir mūrdhā divaḥ kakut,patiḥ pṛthivyā ayam, apāṁ retāṁsi jinvati ', translation:'Fire rise, crown the heavens. Protect this earth. Vitalize the planted seeds.'}}},
+{id:1, day:'Monday', graha:'Moon / Shukra', mantras:{beej:'\t\tOm Kraam Kreem Kraum Saha,\n\t\t\t\t Chandramase Namaha', alt:'Om Chandraya Namaha', translation:'', historic:{mantra:'agnir mūrdhā divaḥ kakut,patiḥ pṛthivyā ayam, apāṁ retāṁsi jinvati ', translation:'agnir mūrdhā divaḥ kakut, patiḥ pṛthivyā ayam, apāṁ retāṁsi jinvati'}}},
+{id:2, day:'Tuesday', graha:'Mars / Mangal', mantras:{beej:'\t\tOm Kraam Kreem Kraum Saha,\n\t\t\t\t Bhaumay Namaha', alt:'Om Mangalaya Namaha', translation:'', historic:{mantra:'agnir mūrdhā divaḥ kakut,patiḥ pṛthivyā ayam, apāṁ retāṁsi jinvati ', translation:'agnir mūrdhā divaḥ kakut, patiḥ pṛthivyā ayam, apāṁ retāṁsi jinvati'}}},
+{id:3, day:'Wednesday', graha:'Mercury / Budha', mantras:{beej:'\t\tOm Braam Breem Braum Saha,\n\t\t\t\t Budhaye Namaha', alt:'Om Budhaya Namaha', translation:'', historic:{mantra:'udbudhya svāgna prati-jāgṛ hi, tvaṁ iṣṭāpūrte saṁsṛjethāmayaṁ ca, asmin sadhasthe adhyuttarāsmin, viśve devā yajamānaś ca sīdata', translation:'O Intellect, awaken and become alertly aware of everything. Then, you can achieve objectives without mistake. Then, you can rise above all this confusion. Illuminate everything with your divinity. This is why I call to you.'}}},
+{id:4, day:'Thursday', graha:'Jupiter / Guruve', mantras:{beej:'\t\tOm Kraam Kreem Kraum Saha,\n\t\t\t\t Chandramase Namaha', alt:'Om Guruve Namaha', translation:'', historic:{mantra:'bṛhaspate ati yadaryo arhād, dyuma dvibhāti kratum ajjaneṣu, yad dīdayac chavasarta prajāta, tadasmasu draviṇaṁ dhehi citram', translation:'The master of expansion [Jupiter] gives the most wonderful just-rewards, out from the smoke and twilight born from the mother of our efforts. Destroying the falsehood and proliferating the truth, and by so doing granting us the most wondrously beautiful wealth.'}}},
+{id:5, day:'Friday', graha:'Venus / Shukra', mantras:{beej:'\t\tOm Draam Dreem Draum Saha,\n\t\t\t\t Shukraye Namaha', alt:'Om Shukryaya Namaha', translation:'', historic:{mantra:'annāt parisruto rasaṁ brahmaṇā, vyapabit kṣatraṁ payaḥ prajāpatiḥ, ṛtena satyam indriyaṁ vipānaṁ, śukram andhasa indrasy endriyam, idaṁ paya, sṛtaṁ madhu', translation:'The bliss which seems to flow from objects, really exists in spirit. The gods drink this to protect themselves from defeat. You can really drink it only when you can perceive true reality. But drinking it gives the vitality to remove the blind darkness from the senses. So drink this milk & honey.'}}},
+{id:6, day:'Saturday', graha:'Saturn / Shani', mantras:{beej:'\t\tOm Kraam Kreem Kraum Saha,\n\t\t\t\t Chandramase Namaha', alt:'Om Shanisaya Namaha', translation:'', historic:{mantra:'śaṁ no devīr abhiṣṭaya, āpo bhavantu pītaye, śaṁ yor abhisravantu naḥ', translation:'Goddess śam, we celebrate you! Let us drink your flowing waters. Śam, flow passionately to us, breaking the damns.'}}},
+];
 
+
+export const MANTRA_INFOTYPE_BEEJ = 'Beej';
+export const MANTRA_INFOTYPE_ALT = 'Alternate';
+export const MANTRA_INFOTYPE_HISTORIC = 'Historic';
+export const MANTRA_INFOTYPE_TRANSLATION = 'Translation';
 
 /*****
 *      Nakshatra Mantras Screen
